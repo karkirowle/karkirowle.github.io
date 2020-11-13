5b5a8ea5b942e776d975824c3517d27a41b4271d
boomkin <szerk.animeblog@gmail.com>
2020-11-13 13:19:25 +0100

    updated stimuli set

--- images/oral_cancer_journal_paper/conversion.sh
+++ images/oral_cancer_journal_paper/conversion.sh
@@ -2,7 +2,7 @@ for file in $(find -name "*.wav")
 do
 	echo $file
 	echo ${file%.*}_.wav
-	sox $file -b 32 ${file%.*}_.wav rate -I 16000 channels 1 dither -s
+	sox $file -t wav -r 16000 -b 16	"${file%.*}_.wav" channels 1 norm -0.1
+	#sox $file -b 32 ${file%.*}_.wav rate -I 16000 channels 1 dither -s
 	mv ${file%.*}_.wav $file
-	#sox $file -t wav -r 16000 -b 16	"${file%.*}_.wav" channels 1
 done
